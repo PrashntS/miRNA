33f7d03052947b2fd9d9622f07bc7e8834297782
Prashant Sinha <prashnts@live.com>
2015-05-14 23:10:13 +0530

    Fixes

--- grapher.py
+++ grapher.py
@@ -1,11 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import networkx as nx
 from miRNA_map import miRNA_map
+import matplotlib.pyplot as plt
 
 G = nx.Graph()
 
+count = 0
+
 for rna, target in miRNA_map.items():
-    print(target)
-    break
+    G.add_node(rna)
+    G.add_nodes_from(target)
+    for gene in target:
+        G.add_edge(rna, gene)
+    count += 1
+
+nx.write_gexf(G,"test_massive.gexf")
