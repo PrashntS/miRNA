02f293b9ba886900e3eafac00fc9a3d355ff6a17
Prashant Sinha <prashnts@live.com>
2015-05-15 13:11:15 +0530

    Fixes

--- grapher.py
+++ grapher.py
@@ -4,6 +4,7 @@
 import networkx as nx
 from miRNA_map import miRNA_map
 import matplotlib.pyplot as plt
+from networkx.algorithms import bipartite
 
 G = nx.Graph()
 
@@ -16,4 +17,7 @@ for rna, target in miRNA_map.items():
         G.add_edge(rna, gene)
     count += 1
 
-nx.write_gexf(G,"test_massive.gexf")
+
+c = bipartite.color(G)
+nx.set_node_attributes(G, 'bipartite', c)
+nx.write_gexf(G,"test_massive_color.gexf")
