272c8fbef9252ce65e3ed3b6b495fbd46b22ead6
Prashant Sinha <prashnts@live.com>
2015-05-16 18:44:06 +0530

    Completely switched to beautiful soup

--- retriever.py
+++ retriever.py
@@ -2,17 +2,39 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from pyquery import PyQuery as pq
+from bs4 import BeautifulSoup
 
 from miRNA_map import miRNA_map
 from map_reverse import miRNA_reverse
 from one_degree_mirna_mirna_map import one_degree_map
 from miRNA_Sequences import sequence_lookup
 
-URL = "http://www.ncbi.nlm.nih.gov"
+def get_gene_summary_homo_sapiens(gene_name):
+    URL = "http://www.ncbi.nlm.nih.gov"
+    payload = {'term': '({0}) AND "Homo sapiens"[porgn:__txid9606]'.format(gene_name) }
+    search_request = requests.get(URL + "/gene", params = payload)
+    gene_dat = BeautifulSoup(search_request.text.encode())
+    gene_url = URL + gene_dat.find("td", {"class": "gene-name-id"}).a.attrs['href']
 
-payload = {'term': '(CCND1) AND "Homo sapiens"[porgn:__txid9606]'}
-r = requests.get(URL + "/gene", params = payload)
-gene_url = URL + pq(pq(r.text.encode())(".gene-tabular-rprt").find(".gene-name-id").eq(0).html()).find('a').attr("href")
+    gene_request = requests.get(gene_url)
+    summary = BeautifulSoup(gene_request.text.encode())
 
-print(requests.get(gene_url).text())
+    head = summary.find("dl").find_all('dt')
+    data = summary.find("dl").find_all('dd')
+
+    saved_summary = {}
+
+    saved_summary['FASTA_URL'] = URL + summary.find("a", {"title": "Nucleotide FASTA report"}).attrs['href']
+    saved_summary['Data_URL'] = gene_request.url
+
+    for i in range(0, max(len(head), len(data))):
+        if "Symbol" in head[i].text:
+            saved_summary['symbol'] = data[i].find(text=True, recursive=False)
+        elif "Name" in head[i].text:
+            saved_summary['name'] = data[i].find(text=True, recursive=False)
+        elif "Summary" in head[i].text:
+            saved_summary['summary'] = data[i].text
+        elif "Gene type" in head[i].text:
+            saved_summary['type'] = data[i].text
+
+    return saved_summary
