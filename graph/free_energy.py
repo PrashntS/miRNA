7fbf3cfb1717e5f215aff26a4db82d104850cdc1
Prashant Sinha <prashnts@live.com>
2015-05-20 14:47:03 +0530

    Fixes

--- graph/free_energy.py
+++ graph/free_energy.py
@@ -6,6 +6,26 @@ import json
 
 from miRNA_map import miRNA_map
 
-miRNA_ID_URI = "http://mirmap.ezlab.org/app/remote/mirna?query={0}&run=1"
-gene_ID_URI  = "http://mirmap.ezlab.org/app/remote/gene?query={0}&run=1"
+
+def routine():
+
+    miRNA_ID_URI = "http://mirmap.ezlab.org/app/remote/mirna?query={0}&run=1"
+    gene_ID_URI  = "http://mirmap.ezlab.org/app/remote/gene?query=dusp&run=1"
+
+    prediction_URI = "http://mirmap.ezlab.org/app/remote/db"
+
+    data = {
+        'json_request': {  
+            "queries":[  
+                {  
+                    "run": 1,
+                    "mirna": 11021,
+                    "gene": 14925
+                }
+            ],
+            "valuesFormat":" raw",
+            "valuesLevel": "pair",
+            "withSequence": True
+        }
+    }
 
