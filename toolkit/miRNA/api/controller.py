10d9e76a596a6a36a7b8c50087e987703b18e8d7
Prashant Sinha <prashant@ducic.ac.in>
2016-01-13 20:09:07 +0530

    Retrieval Models

--- toolkit/miRNA/api/controller.py
+++ toolkit/miRNA/api/controller.py
@@ -8,7 +8,7 @@ from flask.ext.mongorest import MongoRest
 from flask.ext.mongorest.views import ResourceView
 from flask.ext.mongorest import methods
 
-from miRNA.polynucleotide.controller import GeneResource
+from miRNA.polynucleotide.controller import GeneResource, miRNAResource
 from miRNA import app, db
 
 api = Blueprint('api', __name__)
@@ -18,4 +18,9 @@ mongo_rest = MongoRest(app, url_prefix = '/api')
 @mongo_rest.register(name = 'gene', url = '/gene/')
 class GeneRest(ResourceView):
   resource = GeneResource
+  methods = [methods.Fetch, methods.List]
+
+@mongo_rest.register(name = 'miRNA', url = '/mirna/')
+class miRNAREST(ResourceView):
+  resource = miRNAResource
   methods = [methods.Fetch, methods.List]
--- toolkit/miRNA/polynucleotide/controller.py
+++ toolkit/miRNA/polynucleotide/controller.py
@@ -5,11 +5,22 @@
 from flask.ext.mongorest import operators as ops
 from flask.ext.mongorest.resources import Resource
 
-from miRNA.polynucleotide.model import Gene, miRNA
+from miRNA.polynucleotide.model import Gene, miRNA, miRNAGeneTargetComplex
 
 class GeneResource(Resource):
-    document = Gene
+  document = Gene
 
-    filters = {
-        'symbol': [ops.Exact, ops.Startswith],
-    }
+  filters = {
+    'symbol': [ops.Exact, ops.Startswith],
+  }
+
+class miRNAResource(Resource):
+  document = miRNA
+
+  related_resources = {
+    'targets': miRNAGeneTargetComplex
+  }
+
+  filters = {
+    'symbol': [ops.Exact, ops.Startswith],
+  }
