a3d8e5ff567e5a564801da2731083d15804bc9aa
Prashant Sinha <prashant@ducic.ac.in>
2016-01-29 02:17:27 +0530

    NCBI Scraper

--- toolkit/miRNA/procedure/scraper.py
+++ toolkit/miRNA/procedure/scraper.py
@@ -4,7 +4,9 @@
 
 import asyncio
 import aiohttp
+
 from tqdm import tqdm
+from pyquery import PyQuery as pq
 
 class Retriever(object):
   def __init__(self, ids):
@@ -45,9 +47,43 @@ class NCBIGene(Retriever):
     return 'http://www.ncbi.nlm.nih.gov/gene/{0}'.format(_id)
 
   def callback(self, dat):
-    return dat[:100]
+    d = pq(dat.encode())
+
+    s_dl = d("[id=summaryDl]").eq(0).children()
+    i_pw = d(".geneinfo-subsec.infopathway").children()
+
+    i_sec = pq(d(".gene-ontology.infosec").html()).find('tbody')
+    funct = i_sec.eq(0).children()
+    procs = i_sec.eq(1).children()
+
+    protn = pq(d(".gene-general-protein-info").html()).find('dl')
+
+    pw_gen = lambda x: {
+      'name': x.children().eq(1).children().eq(0).text(),
+      'desc': x.children().eq(1).children().eq(1).text(),
+      'href': x.children().eq(0).attr('href'),
+    }
+
+    l_f = len(funct)
+    l_p = len(procs)
+
+    doc = {
+      'full_name': s_dl.eq(3).text(),
+      'lineage': s_dl.eq(15).text().split('; '),
+      'alias': s_dl.eq(17).text().split('; '),
+      'summary': s_dl.eq(19).text(),
+      'pathways': [pw_gen(i_pw.eq(_)) for _ in range(len(i_pw))],
+      'function': [funct.eq(_).children().eq(0).text() for _ in range(l_f)],
+      'process': [procs.eq(_).children().eq(0).text() for _ in range(l_p)],
+      'protein_info': {
+        'name': protn.eq(0).children().eq(1).text(),
+        'alias': [_.text for _ in protn.eq(1).children()[1:]]
+      }
+    }
+
+    return doc
 
 # ids = ['1026', 'CDKN2A', 'fhdnisjckoz']
 # x = NCBIGene(ids)
 # x.routine()
-# print(x.storage, x.missed)
+# print(x.storage)
