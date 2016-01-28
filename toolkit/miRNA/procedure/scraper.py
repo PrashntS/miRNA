35acee3ee03cf0404e2adfa890b3ca93cc29a396
Prashant Sinha <prashant@ducic.ac.in>
2016-01-28 23:56:41 +0530

    Scraper basic scaffold.

--- toolkit/miRNA/procedure/scraper.py
+++ toolkit/miRNA/procedure/scraper.py
@@ -10,22 +10,28 @@ class Retriever(object):
   def __init__(self, ids):
     self.conn = aiohttp.connector.TCPConnector(limit=10)
     self.ids = ids
+    self.missed = []
     self.storage = {}
 
   async def delegator(self, coros):
     for f in tqdm(asyncio.as_completed(coros), total=len(coros)):
-      out_dat, _id = await f
-      self.storage[_id] = self.callback(out_dat)
+      out_dat, _id, status = await f
+      if status == 200:
+        self.storage[_id] = self.callback(out_dat)
+      else:
+        self.missed.append(_id)
 
   async def _get(self, _id):
     uri = self.uri_gen(_id)
     ret = await aiohttp.request('GET', uri, connector=self.conn)
-    return await ret.text(), _id
+    return await ret.text(), _id, ret.status
 
   def routine(self):
     loop = asyncio.get_event_loop()
     tasks = [self._get(_) for _ in self.ids]
     loop.run_until_complete(self.delegator(tasks))
+
+  def __del__(self):
     self.conn.close()
 
   def uri_gen(self, _id):
@@ -39,4 +45,9 @@ class NCBIGene(Retriever):
     return 'http://www.ncbi.nlm.nih.gov/gene/{0}'.format(_id)
 
   def callback(self, dat):
-    return "Test"
+    return dat[:100]
+
+# ids = ['1026', 'CDKN2A', 'fhdnisjckoz']
+# x = NCBIGene(ids)
+# x.routine()
+# print(x.storage, x.missed)
