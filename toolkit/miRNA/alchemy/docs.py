6d0448a9bcc5877920f54515caabda21a3460652
Prashant Sinha <prashant@ducic.ac.in>
2016-02-22 01:47:24 +0530

    Added a Database Abstraction for the Gene, miRNA documents.

---
+++ toolkit/miRNA/alchemy/docs.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#.--. .-. ... .... -. - ... .-.-.- .. -.
+
+from packrat import db
+from miRNA.graph import g
+
+class Gene(object):
+  def __init__(self, gene_id):
+    coll = db['ncbi_gene_docs']
+    self.gid = gene_id
+    self.doc = coll.find_one({'gene_id': gene_id})
+
+  @property
+  def targets(self):
+    return g.target(self.gid)
+
+  @property
+  def synonyms(self):
+    out = self.doc['synonyms']
+    if type(out) is str:
+      out = [out]
+    return out
+
+  @property
+  def repr(self):
+    return {
+      'symbol': self.gid,
+      'name': self.doc.get('name'),
+      'synonyms': self.synonyms,
+      'summary': self.doc.get('summary'),
+      'protein_ref': self.doc.get('protein_ref'),
+      'functions': self.doc.get('functions'),
+      'processes': self.doc.get('processes'),
+    }
+
+  @property
+  def sequences(self):
+    seq = db['ensembl_seq'].find_one({'gene_id': self.gid})
+    return seq['fasta']
+
+  @property
+  def canonical(self):
+    return max(self.sequences, key=lambda x: len(x['seq']))['seq']
+
+  @property
+  def host_of(self):
+    return g.host(self.gid)
+
+  @property
+  def targeted_by(self):
+    return g.target(self.gid)
+
+class MiRNA(object):
+  def __init__(self, mir_id):
+    coll = db['mirna_seq']
+    self.mid = mir_id
+
+  @property
+  def sequence(self):
+    seqd = db['mirna_seq'].find_one({'mir_id': self.mid})
+    return seqd['fasta'][0]['seq']
+
+  @property
+  def repr(self):
+    return {
+      'symbol': self.mid,
+    }
+
+  @property
+  def targets(self):
+    return g.target(self.mid)
+
+  @property
+  def host_gene(self):
+    return g.host(self.mid)
--- toolkit/miRNA/stats/thermodynamics.py
+++ toolkit/miRNA/stats/thermodynamics.py
@@ -4,22 +4,15 @@
 
 from miRNA import memcache
 from mirmap import miRmap
-from packrat import c_ensembl_seq, c_mirna_seq
+from miRNA.alchemy.docs import Gene, MiRNA
 
 class Thermodynamics(object):
   def __init__(self, gene_id, mirna_id, **kwargs):
     #: Get Gene Sequences
-    self.gen = c_ensembl_seq.find_one({'gene_id': gene_id})
-    self.mir = c_mirna_seq.find_one({'mir_id': mirna_id})
+    self.gen = Gene(gene_id)
+    self.mir = MiRNA(mirna_id)
     self.__process_seq()
     self.__mirmap_init(**kwargs)
-
-  def __preprocess_gen_seq(self, seqs):
-    key = max(seqs, key=lambda x: len(x['seq']))
-    return key['seq'].replace('T', 'U')
-
-  def __preprocess_mir_seq(self, seqs):
-    return seqs[0]['seq']
 
   def __mirmap_init(self, **kwargs):
     miRmap_args = {
@@ -39,8 +32,8 @@ class Thermodynamics(object):
     self.miRmap = miRmap(**miRmap_args)
 
   def __process_seq(self):
-    self.gen_seq = self.__preprocess_gen_seq(self.gen['fasta'])
-    self.mir_seq = self.__preprocess_mir_seq(self.mir['fasta'])
+    self.gen_seq = self.gen.canonical.replace('T', 'U')
+    self.mir_seq = self.mir.sequence
 
   @property
   def delta_g_binding(self):
@@ -50,54 +43,6 @@ class Thermodynamics(object):
   @property
   def report(self):
     return self.miRmap.report
-
-class ThermodynamicsTranscript(object):
-  def __init__(self, gene_id, mirna_id, **kwargs):
-    #: Get Gene Sequences
-    self.gen = c_ensembl_seq.find_one({'gene_id': gene_id})
-    self.mir = c_mirna_seq.find_one({'mir_id': mirna_id})
-    self.__process_seq()
-    self.__mirmap_init(**kwargs)
-
-  def __preprocess_seq(self, seqs):
-    return ''.join([_['seq'] for _ in seqs]).replace('T', 'U')
-
-  def __preprocess_trans_seq(self, seqs):
-    return {_['id']: _['seq'].replace('T', 'U') for _ in seqs}
-
-  def __process_seq(self):
-    self.mir_seq = self.__preprocess_seq(self.mir['fasta'])
-    self.gen_seq = self.__preprocess_trans_seq(self.gen['fasta'])
-
-  def __mirmap_init(self, **kwargs):
-    miRmap_args = lambda x: {
-      'seq_mir': self.mir_seq,
-      'seq_mrn': x,
-      'seed_args': {
-        'allowed_lengths': [6, 7],
-        'allowed_gu_wobbles': {6: 0, 7: 0},
-        'allowed_mismatches': {6: 0, 7: 0},
-        'take_best': True
-      },
-      'thermo_args': {
-        'temperature': 25.0
-      }
-    }
-    self.miRmaps = {}
-    for transcid, transeq in self.gen_seq.items():
-      miRmap_arg = miRmap_args(transeq)
-      miRmap_arg.update(kwargs.get('miRmap_args', {}))
-      self.miRmaps[transcid] = miRmap(**miRmap_arg)
-
-  @property
-  def delta_g_binding(self):
-    out = {}
-    for transcid, mirmap in self.miRmaps.items():
-      try:
-        out[transcid] = mirmap.thermodynamic_features['dg_binding']
-      except ValueError:
-        out[transcid] = None
-    return out
 
 @memcache.cached()
 def get_delta_g_binding(gene_id, mirna_id):
