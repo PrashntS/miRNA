43b11a65f94f1ffb6118e42a6b30b4b3c53284ec
Prashant Sinha <prashant@ducic.ac.in>
2016-02-28 20:23:56 +0530

    Updated the base class for thermodynamic data calculation

--- toolkit/miriam/stats/thermodynamics.py
+++ toolkit/miriam/stats/thermodynamics.py
@@ -2,10 +2,8 @@
 # -*- coding: utf-8 -*-
 #.--. .-. ... .... -. - ... .-.-.- .. -.
 
-import json
+from metarna.target_scan import free_energy, scan as target_scan
 
-from miriam import memcache
-from pymiranda import find_targets as miranda_targets
 from miriam.alchemy.docs import Gene, MiRNA
 
 class Thermodynamics(object):
@@ -23,29 +21,12 @@ class Thermodynamics(object):
     }
     self.miranda_args.update(kwargs.get("miranda_args", {}))
 
-  def __obtain_args(self, gene_id, mirna_id):
-    gen = Gene(gene_id)
-    mir = MiRNA(mirna_id)
-    self.miranda_args['gene_seq'] = gen.canonical
-    self.miranda_args['mirna_seq'] = mir.sequence[::-1]
-
   def delta_g(self, gene_id, mirna_id):
-    try:
-      gen = Gene(gene_id).canonical
-      mir = MiRNA(mirna_id).sequence[::-1]
-      result = json.loads(miranda_targets(gen, mir, **self.miranda_args))
-      return result['digest']['max_energy']
-    except KeyError:
-      raise ValueError("No targets found with given parameters.")
+    gen = Gene(gene_id).canonical
+    mir = MiRNA(mirna_id).sequence
+    return free_energy(gen, mir, **self.miranda_args)
 
-  @property
-  def report(self):
-    return self.result
-
-@memcache.cached()
-def get_delta_g_binding(gene_id, mirna_id):
-  try:
-    obj = Thermodynamics(gene_id, mirna_id)
-    return obj.delta_g_binding
-  except ValueError:
-    return 10
+  def report(self, gene_id, mirna_id):
+    gen = Gene(gene_id).canonical
+    mir = MiRNA(mirna_id).sequence
+    return target_scan(gen, mir, **self.miranda_args)
