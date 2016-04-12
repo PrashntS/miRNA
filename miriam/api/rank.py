84ce704088ad5273c7e6a29d2cc86aab1c33af9b
Prashant Sinha <prashant@ducic.ac.in>
2016-04-12 17:16:13 +0530

    Added API Routes for Rank

--- miriam/api/rank.py
+++ miriam/api/rank.py
@@ -2,7 +2,24 @@
 # -*- coding: utf-8 -*-
 # MiRiam
 import hug
+import falcon
 
-@hug.get('/')
-def get_root():
-  return 'Test'
+from miriam.alchemy.rank import TissueCollection, Tissue
+from miriam.api.directives import id_format, kind
+
+@hug.get('/tissues')
+def get_tissues():
+  tissues = TissueCollection()
+  return tissues.repr
+
+@hug.get('/tissues/{tissue_id}')
+def get_tissues_via_id(tissue_id: id_format):
+  try:
+    tissue = Tissue(tissue_id)
+    return tissue.repr
+  except KeyError:
+    raise falcon.HTTPNotFound
+
+@hug.get('/{kind}/{tissue_id}')
+def get_rank(tissue_id: id_format, kind: kind):
+  return 'test'
